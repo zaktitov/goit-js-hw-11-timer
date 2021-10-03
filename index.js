df55891471acd7e4115a2832da6741dbdb055556
zaktitov <zaktitov@gmail.com>
2021-10-03 16:11:57 +0300

    styles&html

---
+++ index.js
@@ -0,0 +1,10 @@
+const refs = {
+  countdownWrapper: document.getElementById("timer-1"),
+  days: document.querySelector(`[data-value="days"]`),
+  hours: document.querySelector(`[data-value="hours"]`),
+  mins: document.querySelector(`[data-value="mins"]`),
+  secs: document.querySelector(`[data-value="secs"]`),
+};
+
+const { countdownWrapper,days, hours, mins, secs } = refs;
+
