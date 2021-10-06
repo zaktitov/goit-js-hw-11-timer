6d0785e2cb4686f95e7f570f7c7ed64855c6c04e
zaktitov <zaktitov@gmail.com>
2021-10-06 20:35:34 +0300

    0609

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
-const refs = {
-  countdownWrapper: document.getElementById("timer-1"),
-  day: document.querySelector(`[data-value="days"]`),
-  hour: document.querySelector(`[data-value="hours"]`),
-  min: document.querySelector(`[data-value="mins"]`),
-  sec: document.querySelector(`[data-value="secs"]`),
-};
-const { countdownWrapper, day, hour, min, sec } = refs;
-
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector;
     this.targetDate = targetDate;
   }
 
-  countdown() {
+  pad(value) {
+    return String(value).padStart(2, `0`);
+  }
+
+  start() {
     setInterval(() => {
       this.currentDate = new Date().getTime();
-      this.interim = this.targetDate - this.currentDate;
+      this.time = this.targetDate - this.currentDate;
 
-      function pad(value) {
-        return String(value).padStart(2, `0`);
-      }
-
-      const days = pad(Math.floor(this.interim / (1000 * 60 * 60 * 24)));
-      const hours = pad(
-        Math.floor((this.interim % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      const days = this.pad(Math.floor(this.time / (1000 * 60 * 60 * 24)));
+      const hours = this.pad(
+        Math.floor((this.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
       );
-      const mins = pad(
-        Math.floor((this.interim % (1000 * 60 * 60)) / (1000 * 60))
+      const mins = this.pad(
+        Math.floor((this.time % (1000 * 60 * 60)) / (1000 * 60))
       );
-      const secs = pad(Math.floor((this.interim % (1000 * 60)) / 1000));
+      const secs = this.pad(Math.floor((this.time % (1000 * 60)) / 1000));
 
-      refs.day.textContent = days;
-      refs.hour.textContent = hours;
-      refs.min.textContent = mins;
-      refs.sec.textContent = secs;
+      document.querySelector(`[data-value="days"]`).textContent = days;
+      document.querySelector(`[data-value="hours"]`).textContent = hours;
+      document.querySelector(`[data-value="mins"]`).textContent = mins;
+      document.querySelector(`[data-value="secs"]`).textContent = secs;
     }, 1000);
   }
 }
 
 const newTimer = new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date("Oct 17, 2021").getTime(),
+  targetDate: new Date("Oct 10, 2021").getTime(),
 });
 
-console.log(newTimer.countdown());
+newTimer.start()
