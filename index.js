f106deaeb558907299ab366fcd6a777f67abe159
zaktitov <zaktitov@gmail.com>
2021-10-08 17:39:31 +0300

    0810

--- index.js
+++ index.js
@@ -9,40 +9,10 @@ class CountdownTimer {
   }
 
   start() {
-    // document.querySelector("body").insertAdjacentHTML(
-    //   "afterbegin",
-    //   `<div class="timer" id=${this.selector}">
-    //     <div class="field">
-    //       <span class="value" data-value="days">00</span>
-    //       <span class="label">Days</span>
-    //     </div>
-
-    //     <div class="field">
-    //       <span class="value" data-value="hours">
-    //         00
-    //       </span>
-    //       <span class="label">Hours</span>
-    //     </div>
-
-    //     <div class="field">
-    //       <span class="value" data-value="mins">
-    //         00
-    //       </span>
-    //       <span class="label">Minutes</span>
-    //     </div>
-
-    //     <div class="field">
-    //       <span class="value" data-value="secs">
-    //         00
-    //       </span>
-    //       <span class="label">Seconds</span>
-    //     </div>
-    //   </div>`
-    // );
-    
     document.querySelector("div.timer").setAttribute("id", this.selector);
 
     setInterval(() => {
+      
       this.currentDate = new Date().getTime();
       this.time = this.targetDate - this.currentDate;
 
